65755f6c5980f0bf5a332291bffabdceeb6ee9c0
KidziaK <KidziaK@MIKOAJ>
2018-02-03 22:24:25 +0100

    First

---
+++ LeavesDetection/LeavesDetection/Leaves.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace LeavesDetection
+{
+    partial class Leaves
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label1;
+            this.InputImage = new Emgu.CV.UI.ImageBox();
+            this.OutputImage = new Emgu.CV.UI.ImageBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TresholdBar = new System.Windows.Forms.TrackBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.InputImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TresholdBar)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(241, 146);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(78, 13);
+            label1.TabIndex = 7;
+            label1.Text = "Treshold Value";
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // InputImage
+            // 
+            this.InputImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.InputImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InputImage.Location = new System.Drawing.Point(3, 3);
+            this.InputImage.Name = "InputImage";
+            this.InputImage.Size = new System.Drawing.Size(560, 446);
+            this.InputImage.TabIndex = 2;
+            this.InputImage.TabStop = false;
+            // 
+            // OutputImage
+            // 
+            this.OutputImage.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.OutputImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OutputImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.OutputImage.Location = new System.Drawing.Point(569, 3);
+            this.OutputImage.Name = "OutputImage";
+            this.OutputImage.Size = new System.Drawing.Size(585, 446);
+            this.OutputImage.TabIndex = 2;
+            this.OutputImage.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(569, 455);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(585, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(548, 131);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Find leaf";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TresholdBar
+            // 
+            this.TresholdBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TresholdBar.Location = new System.Drawing.Point(3, 165);
+            this.TresholdBar.Maximum = 255;
+            this.TresholdBar.Name = "TresholdBar";
+            this.TresholdBar.Size = new System.Drawing.Size(554, 31);
+            this.TresholdBar.TabIndex = 6;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.91962F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 51.08038F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.InputImage, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.OutputImage, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 68.90625F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 31.09375F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1157, 657);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.TresholdBar, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(label1, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 455);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.82051F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.17949F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(560, 199);
+            this.tableLayoutPanel2.TabIndex = 9;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(554, 137);
+            this.tableLayoutPanel3.TabIndex = 10;
+            // 
+            // Leaves
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 657);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Leaves";
+            this.Text = "Leaves";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.InputImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TresholdBar)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox InputImage;
+        private Emgu.CV.UI.ImageBox OutputImage;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar TresholdBar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    }
+}
+
---
+++ LeavesDetection/LeavesDetection/Leaves.cs
@@ -0,0 +1,114 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeavesDetection
+{
+    public partial class Leaves : Form
+    {
+        public Leaves()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InputImage_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte> colordImage = (Image<Bgr, Byte>)InputImage.Image;
+            Image<Bgr, Byte> temp = colordImage.Copy();
+            temp._EqualizeHist();
+            temp._SmoothGaussian(9);
+
+            Image<Gray, Byte> grayImage = temp.InRange(new Bgr(0, 40, 0), new Bgr(135, 256, 135));
+            grayImage = grayImage.Erode(4);
+            grayImage = grayImage.Dilate(4);
+
+            Image<Gray, Byte> grayImage2 = grayImage.Copy();
+
+            using (var contours = new VectorOfVectorOfPoint())
+            {
+
+                CvInvoke.FindContours(grayImage2, contours, null, RetrType.External, ChainApproxMethod.ChainApproxNone);
+
+                for (int i = 0; i < contours.Size; i++)
+                {
+                    if (CvInvoke.ContourArea(contours[i], false) < 50000)
+                    {
+                        CvInvoke.DrawContours(grayImage, contours, i, new MCvScalar(), -1);
+                    }
+                }
+
+            }
+
+            grayImage = grayImage.Erode(4);
+            grayImage = grayImage.Dilate(4);
+
+            grayImage2 = grayImage.Copy();
+
+            using (var contours = new VectorOfVectorOfPoint())
+            {
+
+                CvInvoke.FindContours(grayImage2, contours, null, RetrType.External, ChainApproxMethod.ChainApproxNone);
+
+                double compactness = Math.Pow(CvInvoke.ArcLength(contours[0], true), 2) / (CvInvoke.ContourArea(contours[0], false) * 4 * Math.PI);
+                if (compactness < 2.5f)
+                {
+                    MCvMoments moments = CvInvoke.Moments(grayImage, true);
+
+                    if (moments.M00 < 70000)
+                    {
+                        textBox1.Text = "Leaf is of type 3 ";
+                    }
+                    else
+                    {
+                        textBox1.Text = "Leaf is of type 2 ";
+                    }
+                }
+
+
+                else
+                {
+                    textBox1.Text = "Leaf is of type 1 ";
+                }
+
+            }
+
+            OutputImage.Image = colordImage.Copy(grayImage);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
